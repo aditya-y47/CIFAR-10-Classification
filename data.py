0327cc87f9f84a31baea7a23d74252235ead6493
Aditya <aditya@Mac-m1.local>
2023-02-25 18:17:34 +0530

    changes

--- data.py
+++ data.py
@@ -2,6 +2,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+
 from config import *
 
 # Custom Transformation Function to Normalize the data this process is done in the Dataloading Stage
@@ -9,20 +10,18 @@ transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 )
 
-batch_size = BATCH_SIZE
-
 trainset = torchvision.datasets.CIFAR10(
     root="./data", train=True, download=True, transform=transform
 )
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=batch_size, shuffle=True, num_workers=2
+    trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2
 )
 
 testset = torchvision.datasets.CIFAR10(
     root="./data", train=False, download=True, transform=transform
 )
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=batch_size, shuffle=False, num_workers=2
+    testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2
 )
 
 classes = (
--- model.py
+++ model.py
@@ -1,18 +1,10 @@
-# Neural Network Structure defined as Net, taking inheretance from nn.Module from Pytorch Library
-# The Code strucutre has been slightly altered to five better results
-
 import os
 
 import torch
-
-# importing libraries to help with NN definations
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-
-from config import *
-from data import testloader, trainloader
 
 
 class Net(nn.Module):
@@ -78,62 +70,5 @@ class Net(nn.Module):
             return self.pool(self.conv6(x))
 
 
-if __name__ == "__main__":
-    model = Net()
-    model.to(device)  # Sending the Model to device for training
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    loss_fn = nn.CrossEntropyLoss()
-
-    # Define a learning rate scheduler
-    scheduler = ReduceLROnPlateau(
-        optimizer, mode="min", factor=0.1, patience=3, verbose=True
-    )
-
-    for epoch in range(NUM_EPOCHS):
-        # Training
-        model.train()
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            inputs, labels = data[0].to(device), data[1].to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            if i % LOG_INTERVAL == LOG_INTERVAL - 1:
-                print(
-                    f"[{epoch + 1}, {i + 1}/{len(trainloader)}] loss: {running_loss / LOG_INTERVAL:.5f}"
-                )
-                running_loss = 0.0
-
-        if ((epoch + 1) % VALIDATION_INTERVAL) == 0:
-            # Validation
-            model.eval()
-            correct = 0
-            total = 0
-            with torch.no_grad():
-                for data in testloader:
-                    images, labels = data[0].to(device), data[1].to(device)
-                    outputs = model(images)
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
-
-            accuracy = 100 * correct / total
-            print(f"+++++" * 10)
-            print("Finished Validation")
-            print(f"Validation accuracy: {accuracy}%")
-            print(f"+++++" * 10)
-
-            # Learning rate scheduler step
-            scheduler.step(running_loss)
-    print(f"+++++" * 10)
-    print("Finished Training")
-    print(f"+++++" * 10)
-    if os.path.isdir("./models"):
-        pass
-    else:
-        os.mkdir("./models")
-    torch.save(model.state_dict(), EXPORT_PATH)
+class TransferInceptionNetV3(nn.Module):
+    pass
